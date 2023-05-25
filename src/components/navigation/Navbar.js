c44cddc6f38a5e0737c947fc01ef4868b0c8179a
mangelvl <thorristerki@gmail.com>
2023-05-25 17:00:13 -0400

    add css button navbar

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,22 +1,36 @@
 import { connect } from 'react-redux'
-
+import {Link} from 'react-router-dom'
+import logo_bk from 'assets/img/cat-blackface.png'
+import logo_ot2 from 'assets/img/cat-outline2.png'
 function Navbar() {
     return (
-        <nav className='w-full py-10 shadow-md fixed'>
+        <nav className='w-full py-4 shadow-md fixed'>
             <div className="bg-white px-4 sm:px-6">
-                <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
+                <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
                     <div className="ml-4 mt-2">
-                        <h3 className="text-lg font-medium leading-6 text-gray-900">Job Postings</h3>
+                        <img
+                        src={logo_bk}
+                        width={70}
+                        height={60}
+                        className=""/>
                     </div>
                     <div className="ml-4 mt-2 flex-shrink-0">
-                        <h3 className="text-lg inline-flex font-medium leading-6 text-gray-900 mx-8">Job Postings</h3>
-                        <h3 className="text-lg inline-flex font-medium leading-6 text-gray-900 mx-8">Job Postings</h3>
-                        <h3 className="text-lg inline-flex font-medium leading-6 text-gray-900 mx-8">Job Postings</h3>
+                        <Link to='/casos' className="text-lg inline-flex font-medium leading-6 text-gray-900 hover:underline-orange-500 hover:underline hover:underline-offset-2 mx-4">Casos</Link>
+                        <Link to='/servicios' className="text-lg inline-flex font-medium leading-6 text-gray-900 hover:underline-orange-500 hover:underline hover:underline-offset-2 mx-4">Servicios</Link>
+                        <Link to='/nosotros' className="text-lg inline-flex font-medium leading-6 text-gray-900 hover:underline-orange-500 hover:underline hover:underline-offset-2 mx-4">Nosotros</Link>
+                        <Link to='/carreras' className="text-lg inline-flex font-medium leading-6 text-gray-900 hover:underline-orange-500 hover:underline hover:underline-offset-2 mx-4">Carreras</Link>
+                        <Link to='/blog' className="text-lg inline-flex font-medium leading-6 text-gray-900 hover:underline-orange-500 hover:underline hover:underline-offset-2 mx-4">Blog</Link>
+                        <Link to='/contacto' className="text-lg inline-flex font-medium leading-6 text-gray-900 hover:underline-orange-500 hover:underline hover:underline-offset-2 mx-4">Contacto</Link>
                         <button
                             type="button"
-                            className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-orange-button px-6 py-2 text-lg font-bold text-white shadow-sm hover:transition duration-300 ease-out hover:bg-black focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
                         >
-                            Create new job
+                            Hire us
+                            <img
+                            src={logo_ot2} className='mt-1 ml-2'
+                            width={20}
+                            height={20}
+                            />
                         </button>
                     </div>
                 </div>

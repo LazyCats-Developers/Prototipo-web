052ba2c4e96667f65467c826b413b9c42dcc487c
mangelvl <thorristerki@gmail.com>
2023-05-25 23:41:04 -0400

    Sin asco >:(

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,11 +1,30 @@
 import { connect } from 'react-redux'
 import {Link} from 'react-router-dom'
+import {useState, Fragment, useEffect } from 'react'
 import logo_bk from 'assets/img/cat-blackface.png'
 import logo_ot2 from 'assets/img/cat-outline2.png'
+import DotLoader from 'react-spinners/DotLoader'
+
 function Navbar() {
+    
+    const [loading,setLoading]=useState(true)
+    window.onscroll = function() {scrollFunction()}
+
+    function scrollFunction() {
+        if(document.getElementById('navbar')){
+            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+                document.getElementById('navbar').classList.add('shadow-navbar');
+                document.getElementById('navbar').classList.add('bg-white');
+            }else{
+                document.getElementById('navbar').classList.remove('shadow-navbar');
+                document.getElementById('navbar').classList.remove('bg-white');
+            }
+        }
+    }
+
     return (
-        <nav className='w-full py-4 top-0 fixed'>
-            <div className="bg-white px-4 sm:px-6">
+        <nav id='navbar' className='w-full py-6 top-0 transition duration-300 ease-in-out z-40 fixed'>
+            <div className="px-4 sm:px-6">
                 <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
                     <div className="ml-4 mt-2">
                         <Link to='/'>
@@ -22,17 +41,14 @@ function Navbar() {
                         <Link to='/carreras' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Carreras</Link>
                         <Link to='/blog' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Blog</Link>
                         <Link to='/contacto' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Contacto</Link>
-                        <button
+                        <Link
+                            to="/contacto"
                             type="button"
-                            className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-orange-button px-6 py-2 text-lg font-bold text-white shadow-sm hover:transition duration-300 ease-out hover:bg-black focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-                        >
+                            className="ml-12 inline-flex items-center rounded-md border border-transparent bg-orange-button px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-black transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
                             Hire us
-                            <img
-                            src={logo_ot2} className='mt-1 ml-2'
-                            width={20}
-                            height={20}
-                            />
-                        </button>
+                            <DotLoader className="ml-3 -mr-1 h-5 w-5" loading={loading} size={15} color='#ffffff'/>
+                        </Link>
                     </div>
                 </div>
             </div>
--- src/containers/pages/Home.jsx
+++ src/containers/pages/Home.jsx
@@ -1,13 +1,16 @@
+import Header from "components/home/Header";
 import Navbar from "components/navigation/Navbar";
 import Footer from "components/navigation/Footer";
 import Layout from "hocs/layouts/Layout";
+import Incentives from "components/home/Incentives";
 
 function Home(){
     return(
         <Layout>
             <Navbar/>
-           <div className="pt-28">
-                <h1 className="text-center text-9xl border pt-10 pb-10">Holiwi</h1>
+            <div className="pt-28">
+                    <Header/>
+                    <Incentives/>
             </div> 
             <Footer/>
         </Layout>

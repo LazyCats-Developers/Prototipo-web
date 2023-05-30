d6049f30e78e319beef540f30485cc79f1b0de06
Humberto Quinan <humbertoquinan@hotmail.com>
2023-05-30 00:13:16 -0400

    ADD SEO and animated Transition

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom'
 import store from './store';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 
-import Error404 from 'containers/errors/Error404';
-import Home from 'containers/pages/Home';
-import Cases from "containers/pages/Cases";
-import Services from "containers/pages/Services";
-import About from "containers/pages/About";
-import Careers from "containers/pages/Careers";
-import Blog from "containers/pages/Blog";
-import Contact from "containers/pages/Contact";
-import Products from "containers/pages/Products";
+
+import AnimatedRoutes from 'AnimatedRoutes';
 
 
 
 function App() {
-  return (
-    <Provider store={store}>
-      
-      <Router>
-          <Routes>
-              {/* Error Display*/}
-              <Route path="*" element={<Error404 />} />
 
-              {/* Home Display*/}
-              <Route path="/" element={<Home />} />
-              <Route path="/casos" element={<Cases />} />
-              <Route path="/Servicios" element={<Services />} />
-              <Route path="/nosotros" element={<About />} />
-              <Route path="/carreras" element={<Careers />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/contacto" element={<Contact />} />
-              <Route path="/products" element={<Products />} />
-          </Routes>
-      </Router>
-    </Provider>
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>LazyCat Developers</title>
+        <meta name="description" content="Pagina principal de LazyCat Developers" />
+        <meta name="keywords" content="" />
+        <meta name="robots" content="all" />
+        <meta name="author" content="LazyCat" />
+        <meta name="publisher" content="LazyCat" />
+        <link rel="canonical" href="/" />
+
+        {/* Social Media Tags */}
+        <meta property="og:title" content='LazyCat Developers' />
+        <meta property="og:description" content='Agencia de software. Servicios de creacion de pagina web y desarrollo de aplicaciones.' />
+        <meta property="og:url" content="" />
+        <meta property="og:image" content='' />
+
+        <meta name="twitter:title" content='LazyCat DevelopersLazyCat Developers' />
+        <meta
+          name="twitter:description"
+          content='Agencia de software. Servicios de creacion de pagina web y desarrollo de aplicaciones.'
+        />
+        <meta name="twitter:image" content='' />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
+      <Provider store={store}>
+        <Router>
+          <AnimatedRoutes />
+        </Router>
+      </Provider>
+    </HelmetProvider>
   );
 }
 

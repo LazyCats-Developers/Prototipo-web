ef02b979d4f07e4fef5cd75313aa3490361a9676
mangelvl <thorristerki@gmail.com>
2023-05-29 23:38:54 -0400

    add: carrers page and new tailwind components :)

--- src/containers/pages/Careers.jsx
+++ src/containers/pages/Careers.jsx
@@ -2,6 +2,11 @@ import Navbar from "components/navigation/Navbar";
 import Footer from "components/navigation/Footer";
 import Layout from "hocs/layouts/Layout";
 import { useEffect } from "react";
+import Header from "components/careers/Header";
+import Testimonial from "components/careers/Testimonial";
+import LogoCloud from "components/careers/LogoCloud";
+import Features from "components/careers/Features";
+import PositionsList from "components/careers/PositionsList";
 
 function Careers(){
     useEffect(()=> {
@@ -10,9 +15,13 @@ function Careers(){
     return(
         <Layout>
             <Navbar/>
-           <div className="pt-28">
-           Careers
-            </div> 
+            <div className="pt-28">
+                <Header/>
+                <Testimonial/>
+                <LogoCloud/>
+                <Features/>
+                <PositionsList/>
+            </div>
             <Footer/>
         </Layout>
     )
